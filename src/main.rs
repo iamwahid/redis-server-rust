157efaa00ac891125785875b9958624341c369fd
Wahid Niam <iam.wahidn@gmail.com>
2024-04-19 10:54:05 +0700

    feat: handle multi connections

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let pong_resp = simple_resp("PONG");
-    
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-                stream.read(&mut buffer).unwrap();
-                if stream.write_all(pong_resp.as_bytes()).is_err() {
-                    println!("Error writing to stream");
+        let worker = thread::spawn(
+            move || {
+                match stream {
+                    Ok(mut stream) => {
+                        let mut buffer = [0; 1024];
+                        stream.read(&mut buffer).unwrap();
+                        if stream.write_all(simple_resp("PONG").as_bytes()).is_err() {
+                            println!("Error writing to stream");
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        );
+        let _ = worker.join();
     }
 }
 
