6d96cd5d810c3ed41a2516384070b310e2e58437
Wahid Niam <iam.wahidn@gmail.com>
2024-04-20 16:25:27 +0700

    feat: handle info replication master_replid and master_repl_offset

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
-use std::net::TcpListener;
 use std::io::{BufRead, Read, Write};
+use std::net::TcpListener;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::{env, env::Args};
@@ -9,17 +9,23 @@ use tokio::time::{Duration, Instant};
 const DEFAULT_PORT: u16 = 6379;
 
 fn main() {
-    let data_store: Arc<Mutex<HashMap<String, DataStoreValue>>> = Arc::new(Mutex::new(HashMap::new()));
+    let data_store: Arc<Mutex<HashMap<String, DataStoreValue>>> =
+        Arc::new(Mutex::new(HashMap::new()));
     // parse cli args
     let parsed = parse_args(env::args());
 
-    let server_repl_config: Arc<Mutex<ServerReplicationConfig>> = Arc::new(Mutex::new(ServerReplicationConfig{role: String::from("master")}));
+    let server_repl_config: Arc<Mutex<ServerReplicationConfig>> =
+        Arc::new(Mutex::new(ServerReplicationConfig {
+            role: String::from("master"),
+            master_replid: String::from("8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"),
+            master_repl_offset: 0,
+        }));
     let mut bind_address = format!("127.0.0.1:{}", DEFAULT_PORT);
     for arg in parsed.into_iter() {
         match arg {
             ServerArg::Port(port) => {
                 bind_address = format!("127.0.0.1:{}", port);
-            },
+            }
             ServerArg::ReplicaOf(_host, _port) => {
                 server_repl_config.lock().unwrap().role = String::from("slave");
             }
@@ -30,28 +36,24 @@ fn main() {
     for stream in listener.incoming() {
         let data_store = data_store.clone();
         let server_repl_config = server_repl_config.clone();
-        let _worker = thread::spawn(
-            move || {
-                match stream {
-                    Ok(mut stream) => loop {
-                        let mut buffer = [0; 1024];
-                        let read_count = stream.read(&mut buffer).unwrap_or_default();
-                        if read_count == 0 {
-                            break;
-                        }
-
-                        let response = process_req(&buffer, data_store.clone(), server_repl_config.clone());
-
-                        if stream.write_all(response.as_bytes()).is_err() {
-                            println!("Error writing to stream");
-                        }
-                    }
-                    Err(e) => {
-                        println!("error: {}", e);
-                    }
+        let _worker = thread::spawn(move || match stream {
+            Ok(mut stream) => loop {
+                let mut buffer = [0; 1024];
+                let read_count = stream.read(&mut buffer).unwrap_or_default();
+                if read_count == 0 {
+                    break;
                 }
+
+                let response = process_req(&buffer, data_store.clone(), server_repl_config.clone());
+
+                if stream.write_all(response.as_bytes()).is_err() {
+                    println!("Error writing to stream");
+                }
+            },
+            Err(e) => {
+                println!("error: {}", e);
             }
-        );
+        });
     }
 }
 
@@ -96,11 +98,13 @@ struct DataStoreValue {
 
 struct ServerReplicationConfig {
     role: String,
+    master_replid: String,
+    master_repl_offset: u8,
 }
 
 fn parse_args(args: Args) -> HashSet<ServerArg> {
     let args: Vec<_> = args.collect();
-    // let parsed 
+    // let parsed
     let mut args_iter = args.into_iter();
     let mut parsed_args: HashSet<ServerArg> = HashSet::new();
     loop {
@@ -113,7 +117,7 @@ fn parse_args(args: Args) -> HashSet<ServerArg> {
                         panic!("Port empty");
                     };
                     parsed_args.insert(ServerArg::Port(port));
-                },
+                }
                 "replicaof" => {
                     let host = args_iter.next().unwrap();
                     let port = if let Some(port) = args_iter.next() {
@@ -122,8 +126,8 @@ fn parse_args(args: Args) -> HashSet<ServerArg> {
                         DEFAULT_PORT
                     };
                     parsed_args.insert(ServerArg::ReplicaOf(host, port));
-                },
-                _ => ()
+                }
+                _ => (),
             }
         } else {
             break;
@@ -133,7 +137,11 @@ fn parse_args(args: Args) -> HashSet<ServerArg> {
 }
 
 // parse buffer as vector
-fn process_req(&buffer: &[u8; 1024], data_store: Arc<Mutex<HashMap<String, DataStoreValue>>>, server_repl_config: Arc<Mutex<ServerReplicationConfig>>) -> String {
+fn process_req(
+    &buffer: &[u8; 1024],
+    data_store: Arc<Mutex<HashMap<String, DataStoreValue>>>,
+    server_repl_config: Arc<Mutex<ServerReplicationConfig>>,
+) -> String {
     let mut response = simple_resp("");
     let command: Vec<_> = buffer
         .lines()
@@ -145,33 +153,34 @@ fn process_req(&buffer: &[u8; 1024], data_store: Arc<Mutex<HashMap<String, DataS
     let command_length = match command.get(0) {
         Some(fi) => {
             if fi.starts_with("*") {
-                let length =  fi.replace("*", "").parse::<i8>().unwrap();
+                let length = fi.replace("*", "").parse::<i8>().unwrap();
                 length
             } else {
                 -1
             }
-        },
-        None => {
-            -1
-        },
+        }
+        None => -1,
     };
 
     let mut command_items = Vec::with_capacity(command_length as usize);
     let mut offset = 1;
     while command_items.len() < command_length as usize {
-        match (command.get(offset as usize), command.get(offset as usize + 1)) {
+        match (
+            command.get(offset as usize),
+            command.get(offset as usize + 1),
+        ) {
             (Some(com1), Some(com2)) => {
                 command_items.push((com1, com2));
-            },
+            }
             _ => {
                 break;
             }
         };
-        offset = offset+2;
+        offset = offset + 2;
     }
 
     // parse command
-    let command : Option<Command> = match command_items.get(0) {
+    let command: Option<Command> = match command_items.get(0) {
         Some((_pre, command)) => {
             let command = *command;
             match command.clone().to_ascii_lowercase().as_str() {
@@ -182,65 +191,59 @@ fn process_req(&buffer: &[u8; 1024], data_store: Arc<Mutex<HashMap<String, DataS
                 "info" => Some(Command::Info),
                 _ => None,
             }
-        },
-        None => {
-            None
         }
+        None => None,
     };
 
     if let Some(command) = command {
         match command {
             Command::Ping => {
                 response = simple_resp("PONG");
-            },
+            }
             Command::Echo => {
                 match command_items.get(1) {
                     Some((_pre, message)) => {
                         let message = *message;
                         response = simple_resp(message.as_str());
-                    },
+                    }
                     None => {
                         println!("no message");
                     }
                 };
-            },
+            }
             Command::Set => {
                 response = simple_resp("OK");
                 let key = match command_items.get(1) {
                     Some((_pre, message)) => {
                         let message = *message;
                         Some(message.clone())
-                    },
-                    None => None
+                    }
+                    None => None,
                 };
                 let value = match command_items.get(2) {
                     Some((_pre, message)) => {
                         let message = *message;
                         Some(message.clone())
-                    },
-                    None => None
+                    }
+                    None => None,
                 };
                 let expiry_type = match command_items.get(3) {
                     Some((_pre, command)) => {
                         let command = *command;
                         match command.to_ascii_lowercase().as_str() {
-                            "ex" => {
-                                Some(SetExpiry::Ex)
-                            },
-                            "px" => {
-                                Some(SetExpiry::Px)
-                            },
-                            _ => None
+                            "ex" => Some(SetExpiry::Ex),
+                            "px" => Some(SetExpiry::Px),
+                            _ => None,
                         }
-                    },
-                    None => None
+                    }
+                    None => None,
                 };
                 let expiry_number = match command_items.get(4) {
                     Some((_pre, expiry)) => {
                         let expiry = *expiry;
                         Some(expiry.clone().parse::<u64>().unwrap_or(0))
-                    },
-                    None => None
+                    }
+                    None => None,
                 };
                 match (key, value, expiry_type, expiry_number) {
                     (Some(key), Some(value), Some(expiry_type), Some(expiry_number)) => {
@@ -249,21 +252,38 @@ fn process_req(&buffer: &[u8; 1024], data_store: Arc<Mutex<HashMap<String, DataS
                             SetExpiry::Ex => expiry_number * 1000,
                             SetExpiry::Px => expiry_number,
                         };
-                        data_store.lock().unwrap().insert(key, DataStoreValue{value, created_at: Instant::now(), expired_in: Some(Duration::from_millis(expiry_number))});
-                    },
+                        data_store.lock().unwrap().insert(
+                            key,
+                            DataStoreValue {
+                                value,
+                                created_at: Instant::now(),
+                                expired_in: Some(Duration::from_millis(expiry_number)),
+                            },
+                        );
+                    }
                     (Some(key), Some(value), None, None) => {
-                        data_store.lock().unwrap().insert(key, DataStoreValue{value, created_at: Instant::now(), expired_in: None});
-                    },
+                        data_store.lock().unwrap().insert(
+                            key,
+                            DataStoreValue {
+                                value,
+                                created_at: Instant::now(),
+                                expired_in: None,
+                            },
+                        );
+                    }
                     _ => (),
                 }
-            },
+            }
             Command::Get => {
                 match command_items.get(1) {
                     Some((_pre, key)) => {
                         let key = *key;
                         let mut data_store = data_store.lock().unwrap();
                         if let Some(value) = data_store.get(key) {
-                            if let (Some(elapsed), Some(expired_in)) = (Instant::now().checked_duration_since(value.created_at), value.expired_in) {
+                            if let (Some(elapsed), Some(expired_in)) = (
+                                Instant::now().checked_duration_since(value.created_at),
+                                value.expired_in,
+                            ) {
                                 if elapsed.as_millis() >= expired_in.as_millis() {
                                     data_store.remove(key);
                                     response = null_resp();
@@ -278,22 +298,31 @@ fn process_req(&buffer: &[u8; 1024], data_store: Arc<Mutex<HashMap<String, DataS
                         } else {
                             response = null_resp();
                         }
-                    },
+                    }
                     None => {
                         response = null_resp();
                     }
                 };
-            },
+            }
             Command::Info => {
                 match command_items.get(1) {
                     Some((_pre, info_type)) => {
                         let info_type = *info_type;
                         if info_type.eq("replication") {
-                            response = bulk_string_resp(format!("role:{}", server_repl_config.lock().unwrap().role).as_str());
+                            let repl_config = server_repl_config.lock().unwrap();
+                            response = bulk_string_resp(
+                                format!(
+                                    "role:{}\r\nmaster_replid:{}\r\nmaster_repl_offset:{}",
+                                    repl_config.role,
+                                    repl_config.master_replid,
+                                    repl_config.master_repl_offset
+                                )
+                                .as_str(),
+                            );
                         } else {
                             response = null_resp();
                         }
-                    },
+                    }
                     None => {
                         println!("no message");
                     }
